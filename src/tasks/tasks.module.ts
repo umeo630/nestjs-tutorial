b06eab8c3f9ab274423ddb1b8d7ca539fa4ebd63
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-12-10 12:31:37 +0900

    add: TasksService追加

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [],
-  providers: [],
+  providers: [TasksService],
 })
 export class TasksModule {}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(Task) // Repositoryを注入
+    private readonly taskRepository: Repository<Task>,
+  ) {}
+}
