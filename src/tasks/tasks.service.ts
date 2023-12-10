dc1941fe44e076585ba916015584c6557399a694
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-12-10 12:42:32 +0900

    add: Task新規作成

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,4 +10,8 @@ export class TasksService {
     @InjectRepository(Task) // Repositoryを注入
     private readonly taskRepository: Repository<Task>,
   ) {}
+
+  create(dto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.save(dto);
+  }
 }
